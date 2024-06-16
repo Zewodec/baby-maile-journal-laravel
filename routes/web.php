92d56e5bbcfb6918e8e7093f339f2b1e92e6d614
Adam <35268127+Zewodec@users.noreply.github.com>
2024-06-17 02:51:04 +0300

    manage middleware auth

--- routes/web.php
+++ routes/web.php
@@ -67,104 +67,100 @@ Route::prefix('/family')->name('family.')->controller(FamilyController::class)->
 });
 
 Route::prefix('/rozvytok-dytyny')->name('rozvytok-dytyny.')->controller(RozvytokDytynyController::class)->group(function () {
-    Route::get('/', 'rozvytokDytynyPage')->name('index');
+    Route::get('/', 'rozvytokDytynyPage')->name('index')->middleware('auth');
 
-    Route::get('/vagitnist', 'vagitnistPage')->name('vagitnist');
-    Route::get('/nemovlya', 'nemovlyaPage')->name('nemovlya');
+    Route::get('/vagitnist', 'vagitnistPage')->name('vagitnist')->middleware('auth');
+    Route::get('/nemovlya', 'nemovlyaPage')->name('nemovlya')->middleware('auth');
 })->middleware('auth');
 
 
 Route::prefix('/trackers')->name('trackers.')->controller(TrackersController::class)->group(function () {
-    Route::get('/', 'trackersPage')->name('index');
+    Route::get('/', 'trackersPage')->name('index')->middleware('auth');
 
 
     Route::prefix('/vagitnist')->name('vagitnist.')->group(function () {
-        Route::get('/', 'trackerMenuVagitnistPage')->name('index');
+        Route::get('/', 'trackerMenuVagitnistPage')->name('index')->middleware('auth');
 
-        Route::get('/vaga', [VagaTrackerController::class, 'vagaVagitnistTrackerPage'])->name('vaga');
-        Route::post('/add-vaga', [VagaTrackerController::class, 'addVaga'])->name('add-vaga');
-        Route::get('/delete-vaga/{vaga_id}', [VagaTrackerController::class, 'deleteVaga'])->name('delete-vaga');
-        Route::get('/poshtovhs', [PoshtovhsTrackerController::class,'poshtovhsVagitnistTrackerPage'])->name('poshtovhs');
-        Route::get('/poshtovhs/{session_id}/', [PoshtovhsTrackerController::class,'poshtovhsVagitnistDetailsTrackerPage'])->name('poshtovhs_details');
-        Route::get('/poshtovhs/{session_id}/{poshtovhs_id}/delete', [PoshtovhsTrackerController::class,'poshtovhsVagitnistDetailsDeleteItemTrackerPage'])->name('poshtovhs_details_delete');
-        Route::post('/add-poshtovhs', [PoshtovhsTrackerController::class,'addPoshtovhs'])->name('add_poshtovhs');
-        Route::get('/delete-poshtovhs/{session_id}', [PoshtovhsTrackerController::class,'deletePoshtovhs'])->name('delete_poshtovhs');
+        Route::get('/vaga', [VagaTrackerController::class, 'vagaVagitnistTrackerPage'])->name('vaga')->middleware('auth');
+        Route::post('/add-vaga', [VagaTrackerController::class, 'addVaga'])->name('add-vaga')->middleware('auth');
+        Route::get('/delete-vaga/{vaga_id}', [VagaTrackerController::class, 'deleteVaga'])->name('delete-vaga')->middleware('auth');
+        Route::get('/poshtovhs', [PoshtovhsTrackerController::class,'poshtovhsVagitnistTrackerPage'])->name('poshtovhs')->middleware('auth');
+        Route::get('/poshtovhs/{session_id}/', [PoshtovhsTrackerController::class,'poshtovhsVagitnistDetailsTrackerPage'])->name('poshtovhs_details')->middleware('auth');
+        Route::get('/poshtovhs/{session_id}/{poshtovhs_id}/delete', [PoshtovhsTrackerController::class,'poshtovhsVagitnistDetailsDeleteItemTrackerPage'])->name('poshtovhs_details_delete')->middleware('auth');
+        Route::post('/add-poshtovhs', [PoshtovhsTrackerController::class,'addPoshtovhs'])->name('add_poshtovhs')->middleware('auth');
+        Route::get('/delete-poshtovhs/{session_id}', [PoshtovhsTrackerController::class,'deletePoshtovhs'])->name('delete_poshtovhs')->middleware('auth');
     });
 
     Route::prefix('/nemovlya')->name('nemovlya.')->group(function () {
-        Route::get('/', 'trackerMenuNemovlyaPage')->name('index');
+        Route::get('/', 'trackerMenuNemovlyaPage')->name('index')->middleware('auth');
 
-        Route::get('/goduvanya', [GoduvanyaTrackerController::class,'goduvanyaTrackerPage'])->name('goduvanya');
-        Route::post('/goduvanya/save/grudy', [GoduvanyaTrackerController::class,'trackGoduvanyaGrudy'])->name('goduvanya.save.grudy');
-        Route::post('/goduvanya/save/plashechka', [GoduvanyaTrackerController::class,'trackGoduvanyaPlashechka'])->name('goduvanya.save.plashechka');
-        Route::post('/goduvanya/save/tverda', [GoduvanyaTrackerController::class,'trackGoduvanyaTverda'])->name('goduvanya.save.tverda');
+        Route::get('/goduvanya', [GoduvanyaTrackerController::class,'goduvanyaTrackerPage'])->name('goduvanya')->middleware('auth');
+        Route::post('/goduvanya/save/grudy', [GoduvanyaTrackerController::class,'trackGoduvanyaGrudy'])->name('goduvanya.save.grudy')->middleware('auth');
+        Route::post('/goduvanya/save/plashechka', [GoduvanyaTrackerController::class,'trackGoduvanyaPlashechka'])->name('goduvanya.save.plashechka')->middleware('auth');
+        Route::post('/goduvanya/save/tverda', [GoduvanyaTrackerController::class,'trackGoduvanyaTverda'])->name('goduvanya.save.tverda')->middleware('auth');
 
-        Route::get('/zcidjuvanya', [ZcidjuvanyaTrackerController::class,'zcidjuvanyaTrackerPage'])->name('zcidjuvanya');
-        Route::post('/zcidjuvanya/save', [ZcidjuvanyaTrackerController::class,'trackZcidjuvanya'])->name('zcidjuvanya.save');
+        Route::get('/zcidjuvanya', [ZcidjuvanyaTrackerController::class,'zcidjuvanyaTrackerPage'])->name('zcidjuvanya')->middleware('auth');
+        Route::post('/zcidjuvanya/save', [ZcidjuvanyaTrackerController::class,'trackZcidjuvanya'])->name('zcidjuvanya.save')->middleware('auth');
 
-        Route::get('/pidguznik', [PidguznikTrackerController::class,'pidguznikTrackerPage'])->name('pidguznik');
-        Route::post('/pidguznik/save', [PidguznikTrackerController::class,'trackPidguznik'])->name('pidguznik.save');
+        Route::get('/pidguznik', [PidguznikTrackerController::class,'pidguznikTrackerPage'])->name('pidguznik')->middleware('auth');
+        Route::post('/pidguznik/save', [PidguznikTrackerController::class,'trackPidguznik'])->name('pidguznik.save')->middleware('auth');
 
-        Route::get('/son', [SonTrackerController::class,'sonTrackerPage'])->name('son');
-        Route::post('/son/save', [SonTrackerController::class,'trackSon'])->name('son.save');
+        Route::get('/son', [SonTrackerController::class,'sonTrackerPage'])->name('son')->middleware('auth');
+        Route::post('/son/save', [SonTrackerController::class,'trackSon'])->name('son.save')->middleware('auth');
 
-        Route::get('/chas-gri', [ChasGriTrackerController::class,'chasGriTrackerPage'])->name('chas-gri');
-        Route::post('/chas-gri/save', [ChasGriTrackerController::class,'trackChasGri'])->name('chas-gri.save');
+        Route::get('/chas-gri', [ChasGriTrackerController::class,'chasGriTrackerPage'])->name('chas-gri')->middleware('auth');
+        Route::post('/chas-gri/save', [ChasGriTrackerController::class,'trackChasGri'])->name('chas-gri.save')->middleware('auth');
 
-        Route::get('/zdorovya', [ZdorovyaTrackerController::class,'zdorovyaTrackerPage'])->name('zdorovya');
-        Route::post('/zdorovya/save/temp', [ZdorovyaTrackerController::class,'trackTemp'])->name('zdorovya.save.temp');
-        Route::post('/zdorovya/save/liky', [ZdorovyaTrackerController::class,'trackliky'])->name('zdorovya.save.liky');
-        Route::post('/zdorovya/save/symptomes', [ZdorovyaTrackerController::class,'trackSymptomes'])->name('zdorovya.save.symptomes');
+        Route::get('/zdorovya', [ZdorovyaTrackerController::class,'zdorovyaTrackerPage'])->name('zdorovya')->middleware('auth');
+        Route::post('/zdorovya/save/temp', [ZdorovyaTrackerController::class,'trackTemp'])->name('zdorovya.save.temp')->middleware('auth');
+        Route::post('/zdorovya/save/liky', [ZdorovyaTrackerController::class,'trackliky'])->name('zdorovya.save.liky')->middleware('auth');
+        Route::post('/zdorovya/save/symptomes', [ZdorovyaTrackerController::class,'trackSymptomes'])->name('zdorovya.save.symptomes')->middleware('auth');
 
 
-        Route::get('/zrostanya', [ZrostanyaTrackerController::class,'zrostanyaTrackerPage'])->name('zrostanya');
-        Route::post('/zrostanya/save', [ZrostanyaTrackerController::class,'trackZrostanya'])->name('zrostanya.save');
+        Route::get('/zrostanya', [ZrostanyaTrackerController::class,'zrostanyaTrackerPage'])->name('zrostanya')->middleware('auth');
+        Route::post('/zrostanya/save', [ZrostanyaTrackerController::class,'trackZrostanya'])->name('zrostanya.save')->middleware('auth');
 
         Route::get('/progulyanka', [ProgulyankaTrackerController::class,'progulyankaTrackerPage'])->name('progulyanka')->middleware('auth');
         Route::post('/progulyanka/save', [ProgulyankaTrackerController::class,'traclProgulyanka'])->name('progulyanka.save')->middleware('auth');
 
-        Route::get('/history', [HistoryTrackerController::class,'historyTrackerPage'])->name('history');
+        Route::get('/history', [HistoryTrackerController::class,'historyTrackerPage'])->name('history')->middleware('auth');
 
     });
 });
 
 Route::prefix('/actual_information')->name('actual_info.')->controller(ActualInfoController::class)->group(function () {
-    Route::get('/', 'actualInfoPage')->name('index');
+    Route::get('/', 'actualInfoPage')->name('index')->middleware('auth');
 
-    Route::get('/check-list', 'checkListPage')->name('check_list');
-    Route::get('/vpravy', 'vpravyPage')->name('vpravy');
-    Route::get('/poshtovhs', 'poshtovhsPage')->name('poshtovhs');
-    Route::get('/goduvanya', 'goduvanyaPage')->name('goduvanya');
-    Route::get('/fiz-psych-rozv-dytyny', 'fizPsychRozvDytynyPage')->name('fiz_psych_rozv_dytyny');
-    Route::get('/doglyad', 'doglyadPage')->name('doglyad');
-    Route::get('/vpravy-dity', 'vpravyDityPage')->name('vpravy_dity');
-    Route::get('/odyag', 'odyagPage')->name('odyag');
+    Route::get('/check-list', 'checkListPage')->name('check_list')->middleware('auth');
+    Route::get('/vpravy', 'vpravyPage')->name('vpravy')->middleware('auth');
+    Route::get('/poshtovhs', 'poshtovhsPage')->name('poshtovhs')->middleware('auth');
+    Route::get('/goduvanya', 'goduvanyaPage')->name('goduvanya')->middleware('auth');
+    Route::get('/fiz-psych-rozv-dytyny', 'fizPsychRozvDytynyPage')->name('fiz_psych_rozv_dytyny')->middleware('auth');
+    Route::get('/doglyad', 'doglyadPage')->name('doglyad')->middleware('auth');
+    Route::get('/vpravy-dity', 'vpravyDityPage')->name('vpravy_dity')->middleware('auth');
+    Route::get('/odyag', 'odyagPage')->name('odyag')->middleware('auth');
 
-})->middleware('auth');
+});
 
 Route::prefix('/important_events')->name('important_events.')->controller(ImportantEventsController::class)->group(function () {
-    Route::get('/', 'eventsPage')->name('index');
+    Route::get('/', 'eventsPage')->name('index')->middleware('auth');
 
-    Route::get('/add', 'addEventPage')->name('add');
-    Route::post('/add', 'addEventRequest')->name('add');
+    Route::get('/add', 'addEventPage')->name('add')->middleware('auth');
+    Route::post('/add', 'addEventRequest')->name('add')->middleware('auth');
 
     Route::get('/delete/{eventID}', 'deleteEvent')->name('delete')->middleware('auth');
-})->middleware('auth');
+});
 
 Route::prefix('/calendar')->name('calendar.')->controller(CalendarController::class)->group(function () {
-    Route::get('/', 'calendarPage')->name('index');
+    Route::get('/', 'calendarPage')->name('index')->middleware('auth');
 
-    Route::get('/remove-image/{image_id?}', 'removeImageFromEvent')->name('remove_image');
-    Route::post('/add-event', 'addEvent')->name('add_event');
-})->middleware('auth');
+    Route::get('/remove-image/{image_id?}', 'removeImageFromEvent')->name('remove_image')->middleware('auth');
+    Route::post('/add-event', 'addEvent')->name('add_event')->middleware('auth');
+});
 
 Route::get('/settings', [SettingsController::class, 'settingsPage'])->name('settings')->middleware('auth');
 Route::post('/settings/save', [SettingsController::class, 'settingsSaveInfo'])->name('settings.save')->middleware('auth');
 Route::get('/settings/deleteMe', [SettingsController::class, 'deleteMe'])->name('settings.deleteMe')->middleware('auth');
 
-Route::get('/speaking-forum', [SpeakingForumController::class, 'speakingForumPage'])->name('speaking_forum');
-//Route::prefix('/auth')->name('auth.')->controller(AuthController::class)->group(function () {
-//    Route::post('/register', 'register')->name('register');
-//    Route::post('/login', 'login')->name('login');
-//    Route::get('/me', 'me')->name('me')->middleware('auth:sanctum');
-//});
+Route::get('/speaking-forum', [SpeakingForumController::class, 'speakingForumPage'])->name('speaking_forum')->middleware('auth');
+
