5a1e3ef599a32f4437c6318213925993846a06ad
KedaSolya <93682655+KedaSolya@users.noreply.github.com>
2024-10-11 03:47:41 +0300

    auth.page

--- resources/views/layouts/header-nav.blade.php
+++ resources/views/layouts/header-nav.blade.php
@@ -5,7 +5,7 @@
             <li><a href="#options-section">Опції</a></li>
             <li><a href="#feedback-section">Відгуки</a></li>
             <li><a href="#faq-section">Часті запитання</a></li>
-            <li><a class="action-button" href="{{route('auth.')}}">Вхід/Реєстрація</a></li>
+            <li><a class="action-button" href="{{route('auth.page')}}">Вхід/Реєстрація</a></li>
         </ul>
     </nav>
 </header>
